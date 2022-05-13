fc28b937daa7abcd71005c8ba6ea2b01a5c8ed8e
tsuccar <tsuccar@users.noreply.github.com>
2022-05-13 14:05:36 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "green",
+  FontSize: "20px",
+  border: "1px solid black"
+};
+
+// later it can be change as so.
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <div>
+    <h1 style={{ color: "red" }}> Hello World!</h1>
+    <h1 style={customStyle}> Hello World!</h1>
+  </div>,
+  document.getElementById("root")
+);
+
+// style expectes {color:"red"} js-object. That ojbect however needs to be
+// pass into a jsx expression {}.
